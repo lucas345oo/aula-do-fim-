e7a8139dbcb2da455bbbc6badef254bb0ac0349e
lucas345oo <163847127+lucas345oo@users.noreply.github.com>
2024-11-18 08:42:51 -0300

    Add files via upload

---
+++ aula10-main/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Geografia',
+    'quantos paises tem no mundo?',
+    'existem 193 países no mundo hoje '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'Em que continente se encontra o maior deserto do mundo?',
+    'Antártida (ou Antártica)'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o nome do rio que atravessa Londres?',
+    ' rio Tâmisa'
+)
